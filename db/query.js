285b5a85683f68053c51a4ea909659f115251342
Ellis Mwekunda <Scareb15@gmail.com>
2017-10-16 12:26:53 -0600

    created 2 get and 1 post request

--- db/query.js
+++ db/query.js
@@ -2,14 +2,18 @@ var db = require('./connection');
 
 function getAll() {
 	// Get all cartoons and return them to the route
+	return db.select('*').from('cartoon')
 }
 
 function getOne(id) {
 	// Get one cartoon by the id and return it
+	return db.select('*').from('cartoon').where('id', id)
 }
 
 function create(cartoon) {
 	// Add a cartoon to the database
+	console.log(cartoon)
+	return db.table('cartoon').insert(cartoon)
 }
 
 function update(id, cartoon) {
--- routes/cartoons.js
+++ routes/cartoons.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
 // Bring in the queries object that has all of the db query functions
-
+let db = require('../db/query')
 // Create the route for get all
-
+router.get('/', (req, res, next) => {
+    db.getAll()
+    .then(cartoons => res.json(cartoons) )
+    .catch(err => next(err));
+})
 
 // Create the route for get one
+router.get('/:id', (req, res, next) => {
+    const id = req.params.id
 
+    db.getOne(id)
+    .then(cartoon => res.json(cartoon))
+    .catch(err => next (err))
+})
 
 // Create the route for creating an item
+
+router.post('/', (req, res, next) => {
+    db.create(req.body)
+    .then(() => res.sendStatus(201))
+    .catch(err => next(err))
+})
 
 
 // Create the route for updating
