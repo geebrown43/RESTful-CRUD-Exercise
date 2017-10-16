5201f68afc5dd9ddd324b3cabf9de669692f46c2
Ellis Mwekunda <Scareb15@gmail.com>
2017-10-16 13:54:10 -0600

    added heroku

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', index);
-app.use('/api/cartoons', cartoons);
+app.use('/api/ cartoons', cartoons);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
